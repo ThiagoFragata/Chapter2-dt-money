d330cfe8364b7aa9539cb4c3ed41ef53c82afb6a
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-07-16 15:43:18 -0400

    feat: adicionando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-export function App() {
-  return (
-    <div className="container">
-      
-    </div>
-  );
-}
+import styled from "styled-components"
 
+const Title = styled.h1 `
+  font-size: 64px;
+  color: #f77f00;
+`
+
+export function App() {
+  return <div className="container">
+    <Title>Hello, World!</Title>
+  </div>;
+}
