1070776aab658ac3f7fe029dda16b8fec94689bd
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-07-19 15:10:00 -0400

    feat: instalando e configurando MirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,33 +1,40 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
-export function TransactionsTable(){
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+export function TransactionsTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, []);
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Conta de luz</td>
-                        <td className="withdraw">-R$ 200,00</td>
-                        <td>Casa</td>
-                        <td>28/02/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    )
-};
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Conta de luz</td>
+            <td className="withdraw">-R$ 200,00</td>
+            <td>Casa</td>
+            <td>28/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
